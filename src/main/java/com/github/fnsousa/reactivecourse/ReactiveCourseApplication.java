b74cc58ce6393f066856932da4cb5b96c509cce1
Felipe Nascimento <felipe_nsousa@outlook.com>
2023-04-07 16:45:48 -0300

    First commit

---
+++ src/main/java/com/github/fnsousa/reactivecourse/ReactiveCourseApplication.java
@@ -0,0 +1,13 @@
+package com.github.fnsousa.reactivecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fnsousa/reactivecourse/ReactiveCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fnsousa.reactivecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
